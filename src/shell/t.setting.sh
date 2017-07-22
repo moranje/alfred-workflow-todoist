5822d9ef71b1b6ee75ab134f086350000bc60ffe
Martien Oranje <martieno@gmail.com>
2017-07-22 20:31:55 +0200

    fix: spelling error

--- src/shell/t.setting.sh
+++ src/shell/t.setting.sh
@@ -9,7 +9,7 @@
 setting_name="$1"
 setting="$2"
 match="$3"
-succes_message="$4"
+success_message="$4"
 error_message="$5"
 settings_path="$6/settings.json"
 
@@ -24,7 +24,7 @@ then
 
   # Replace variable in settings.json
   json_update "$settings" "$setting_name" "$setting" > "$settings_path"
-  notification="$succes_message"
+  notification="$success_message"
 fi
 
 echo -n "$notification"
