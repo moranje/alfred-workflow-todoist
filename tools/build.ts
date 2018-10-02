467837851f59d21746974eb15d52eef671cea139
Martien Oranje <martieno@gmail.com>
2018-10-02 08:56:34 +0200

    fix: invalid .alfredworkflow file

--- tools/build.ts
+++ tools/build.ts
@@ -3,6 +3,8 @@ const zip = require('bestzip')
 
 shell.exec('rollup -c rollup.config.ts', { silent: true })
 shell.exec('typedoc --out docs --target es6 --theme minimal --mode file src', { silent: true })
+shell.exec('ts-node tools/move-files.ts moveFromTemp', { silent: true })
+shell.exec('npm run bump-plist', { silent: true })
 
 zip({
   source: '*',
