01d56d35e2b6ebbb6ba355535defd1e48848583c
Martien Oranje <martieno@gmail.com>
2018-10-03 06:31:54 +0200

    fix: include version number in workflow

---
+++ tools/build-workflow.ts
@@ -0,0 +1,17 @@
+const shell = require('shelljs')
+const zip = require('bestzip')
+
+shell.exec('npm run bump-plist', { silent: true })
+
+zip({
+  source: '*',
+  destination: '"../Alfred Workflow Todoist.alfredworkflow"',
+  cwd: 'dist/workflow/'
+})
+  .then(() => {
+    console.log('all done!')
+  })
+  .catch((err: Error) => {
+    console.error(err.stack)
+    process.exit(1)
+  })
--- tools/build.ts
+++ tools/build.ts
@@ -4,17 +4,3 @@ const zip = require('bestzip')
 shell.exec('rollup -c rollup.config.ts', { silent: true })
 shell.exec('typedoc --out docs --target es6 --theme minimal --mode file src', { silent: true })
 shell.exec('ts-node tools/move-files.ts moveFromTemp', { silent: true })
-shell.exec('npm run bump-plist', { silent: true })
-
-zip({
-  source: '*',
-  destination: '"../Alfred Workflow Todoist.alfredworkflow"',
-  cwd: 'dist/workflow/'
-})
-  .then(() => {
-    console.log('all done!')
-  })
-  .catch((err: Error) => {
-    console.error(err.stack)
-    process.exit(1)
-  })
