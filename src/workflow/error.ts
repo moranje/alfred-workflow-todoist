ed5b4d232aca6e8e085b9483e14be7f8ce776bac
Martien Oranje <martieno@gmail.com>
2018-10-06 21:37:52 +0200

    chore(typings): add type information

--- src/workflow/error.ts
+++ src/workflow/error.ts
@@ -16,6 +16,14 @@ const ERROR_ENV = {
   WORKFLOW_VERSION: plist.parse(readFileSync(`${process.cwd()}/info.plist`, 'utf8')).version
 }
 
+/**
+ * An extension of the base Error that incorperates workflow
+ * environment variables.
+ *
+ * @export
+ * @interface AlfredError
+ * @extends {Error}
+ */
 export interface AlfredError extends Error {
   QUERY: string
   OSX_VERSION: string
