31715d0c8dc0b7c451e67054a121edc5b0e45b8c
Martien Oranje <martieno@gmail.com>
2018-10-07 22:50:53 +0200

    ci: hopefully fix build stalling

--- src/__tests__/alfred-workflow-todoist.spec.ts
+++ src/__tests__/alfred-workflow-todoist.spec.ts
@@ -6,7 +6,7 @@ describe('Integration: Workflow', () => {
   it("'create' should return a parsed json response", () => {
     let query = 'Get milk @15min @at_home #Stuff p1, tomorrow'
 
-    let output = exec(`node alfred-workflow-todoist.js create "${query}"`).stdout
+    let output = exec(`node alfred-workflow-todoist.js create "${query}"`, { silent: true }).stdout
     expect(output).toMatch(
       /{\\\"content\\\":\\\"Get milk\\\",\\\"priority\\\":4,\\\"due_string\\\":\\\"tomorrow\\\"}/
     )
@@ -15,7 +15,8 @@ describe('Integration: Workflow', () => {
   it("'settings' should return a parsed json response", () => {
     let query = 'token 0123456789abcde0123456789abcde01234'
 
-    let output = exec(`node alfred-workflow-todoist.js settings "${query}"`).stdout
+    let output = exec(`node alfred-workflow-todoist.js settings "${query}"`, { silent: true })
+      .stdout
     expect(output).toMatch(
       /{\\\"key\\\":\\\"token\\\",\\\"value\\\":\\\"0123456789abcde0123456789abcde01234\\\"}/
     )
