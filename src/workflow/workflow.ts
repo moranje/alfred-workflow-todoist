53b13ccf11b6cb22c750ddcc6079ef187bf3e84f
Martien Oranje <martieno@gmail.com>
2018-10-07 14:33:43 +0200

    fix: add missing refrence

--- src/workflow/workflow.ts
+++ src/workflow/workflow.ts
@@ -154,8 +154,18 @@ export const View = compose({
   }
 })
 
-export const Query = compose({
-  // init() {},
+/**
+ * Generate a UUID.
+ *
+ * @author moranje
+ * @since  2016-07-03
+ * @return {String}
+ */
+export function uuid(): string {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    const r = (Math.random() * 16) | 0
 
-  methods: {}
-})
+    const v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
+}
