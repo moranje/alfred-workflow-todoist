079d6fc3e320fd724060609b30ca4d5df4eebc22
Martien Oranje <martieno@gmail.com>
2018-10-07 22:00:21 +0200

    test: add integration tests for `create` and `settings` commands

---
+++ src/__tests__/alfred-workflow-todoist.spec.ts
@@ -0,0 +1,23 @@
+import { exec } from 'shelljs'
+
+process.chdir('dist/workflow')
+
+describe('Integration: Workflow', () => {
+  it("'create' should return a parsed json response", () => {
+    let query = 'Get milk @15min @at_home #Stuff p1, tomorrow'
+
+    let output = exec(`node alfred-workflow-todoist.js create "${query}"`).stdout
+    expect(output).toMatch(
+      /{\\\"content\\\":\\\"Get milk\\\",\\\"priority\\\":4,\\\"due_string\\\":\\\"tomorrow\\\"}/
+    )
+  })
+
+  it("'settings' should return a parsed json response", () => {
+    let query = 'token 0123456789abcde0123456789abcde01234'
+
+    let output = exec(`node alfred-workflow-todoist.js settings "${query}"`).stdout
+    expect(output).toMatch(
+      /{\\\"key\\\":\\\"token\\\",\\\"value\\\":\\\"0123456789abcde0123456789abcde01234\\\"}/
+    )
+  })
+})
