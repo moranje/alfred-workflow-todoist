2478975be401b420aa84bfc31496da648a1d4ddc
Martien Oranje <martieno@gmail.com>
2020-01-30 14:12:38 +0100

    chore(project): prune unused files

--- src/todoist/index.ts
+++
@@ -1,6 +0,0 @@
-export { Task, TaskList, TaskRequest } from './task';
-export { Project, ProjectList } from './project';
-export { Label, LabelList } from './label';
-export { Query } from './query';
-export { parser } from './parser';
-export { TaskAdapter, ProjectAdapter, LabelAdapter } from './rest-api-v8';
