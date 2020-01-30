d24e0f0d785a135c4b1247ed1b0a95a8b51ea444
Martien Oranje <martieno@gmail.com>
2020-01-30 22:58:45 +0100

    chore(project): update rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,55 +1,59 @@
-import commonjs from 'rollup-plugin-commonjs'
-import json from 'rollup-plugin-json'
-import resolve from 'rollup-plugin-node-resolve'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import typescript from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import typescript from '@wessberg/rollup-plugin-ts';
+import builtin from 'builtin-modules';
+import filesize from 'rollup-plugin-filesize';
+import externals from 'rollup-plugin-node-externals';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import { terser } from 'rollup-plugin-terser';
 
-const pkg = require('./package.json')
+const pkg = require('./package.json');
 
-const libraryName = 'alfred-workflow-todoist'
+const libraryName = 'alfred-workflow-todoist';
+
+let plugins = [
+  replace({
+    // Needed for Conf
+    'commonjsRequire.cache': 'require.cache',
+    '../vendor/mac.noindex/': './notifier/',
+  }),
+
+  externals(),
+
+  // Allow json resolution
+  json(),
+
+  resolve({
+    mainFields: ['main'],
+  }),
+
+  typescript({
+    transpiler: 'babel',
+  }),
+
+  commonjs({
+    // ignore: ['require'],
+  }),
+
+  sourceMaps(),
+
+  filesize(),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins = [...plugins, terser()];
+}
 
 export default {
   input: `src/${libraryName}.ts`,
-  output: [{ file: pkg.main, format: 'cjs', sourcemap: true }],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [
-    'assert',
-    'child_process',
-    'constants',
-    'crypto',
-    'events',
-    'fs',
-    'http',
-    'https',
-    'net',
-    'path',
-    'stream',
-    'url',
-    'util',
-    'os',
-    'worker_treads',
-    'zlib'
-  ],
+  output: [{ file: pkg.main, format: 'cjs', sourcemap: 'inline' }],
+  treeshake: true,
   watch: {
-    include: 'src/**'
+    include: 'src/**',
   },
-  plugins: [
-    // Allow json resolution
-    json(),
-
-    // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
-
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs({
-      ignore: ['worker_threads']
-    }),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps()
-  ]
-}
+  external: builtin,
+  inlineDynamicImports: true,
+  plugins: plugins,
+};
