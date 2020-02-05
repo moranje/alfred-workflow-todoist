6ed5f141f8f7ecf362fc6e13597539017b34fdeb
Martien Oranje <martieno@gmail.com>
2020-02-05 22:13:06 +0100

    test: lower coverage requirements for alpha branch

--- jest.config.js
+++ jest.config.js
@@ -47,10 +47,10 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 85,
-      lines: 85,
-      statements: 85,
+      branches: 60,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 
