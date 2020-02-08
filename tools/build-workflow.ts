ac02c22f62e2ebbc0e2a69d4878d58b867e367a6
Martien Oranje <martieno@gmail.com>
2020-02-08 12:40:24 +0100

    fix(build): possibly fix version number not updated in alfred workflow

--- tools/build-workflow.ts
+++ tools/build-workflow.ts
@@ -1,14 +1,22 @@
 import { exec } from 'shelljs';
 import { zip } from 'zip-a-folder';
+import chalk = require('chalk');
 
-exec('npm run bump-version', { silent: true });
+let err = exec('npm run bump-version', { silent: true }).stderr;
+
+if (err) {
+  console.error(`Could not bump version number:\n\n${chalk.red(err)}`);
+  process.exit(1);
+}
 
 zip(
   `${process.cwd()}/dist/workflow`,
   `${process.cwd()}/dist/Alfred Workflow Todoist.alfredworkflow`
 )
   .then(() => {
-    console.log('all done!');
+    console.log(
+      `Done: created ${chalk.green(`Alfred Workflow Todoist.alfredworkflow`)}`
+    );
   })
   .catch((err: Error) => {
     console.error(err.stack);
