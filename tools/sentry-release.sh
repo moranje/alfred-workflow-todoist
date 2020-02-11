000a37c8a8f96ea45ff0da4f192bfe98f0744a34
Martien Oranje <martieno@gmail.com>
2020-02-11 21:56:19 +0100

    chore: add relative location name to workflow entry for sentry

--- tools/sentry-release.sh
+++ tools/sentry-release.sh
@@ -3,6 +3,6 @@ VERSION="$1"
 npx sentry-cli releases new "$VERSION" --finalize
 # npx sentry-cli releases set-commits "$VERSION" --commit
 # moranje/alfred-workflow-todoist
-npx sentry-cli releases files "$VERSION" upload ./dist/workflow/alfred-workflow-todoist.js
+npx sentry-cli releases files "$VERSION" upload ./dist/workflow/alfred-workflow-todoist.js '~/alfred-workflow-todoist.js'
 npx sentry-cli releases files "$VERSION" upload-sourcemaps ./dist/workflow/alfred-workflow-todoist.js.map --rewrite
 # sentry-cli releases deploys "$VERSION" new -e production
