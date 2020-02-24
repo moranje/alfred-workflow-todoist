86e426fcf57bbc56479d15f676c56a4ec2437810
codefactor-io <support@codefactor.io>
2020-02-24 02:04:09 +0000

    [CodeFactor] Apply fixes to commit b719ae1
    [ci skip] [skip ci]

--- src/todoist/project.ts
+++ src/todoist/project.ts
@@ -1,6 +1,6 @@
-import { AlfredError } from '@/project';
-import { Item, List } from '@/workflow';
-import compose from 'stampit';
+import { AlfredError } from '@/project'
+import { Item, List } from '@/workflow'
+import compose from 'stampit'
 
 /** @hidden */
 export const Project: todoist.ProjectFactory = compose({
@@ -18,25 +18,22 @@ export const Project: todoist.ProjectFactory = compose({
 })
 
 /** @hidden */
-export const ProjectList: todoist.ProjectListFactory = compose(
-  List,
-  {
-    init(
-      this: todoist.ProjectListInstance,
-      { projects = [], query }: { projects: todoist.Project[]; query: string }
-    ) {
-      projects.forEach((project: todoist.Project) => {
-        let name = project.name.indexOf(' ') !== -1 ? `[${project.name}]` : project.name
+export const ProjectList: todoist.ProjectListFactory = compose(List, {
+  init(
+    this: todoist.ProjectListInstance,
+    { projects = [], query }: { projects: todoist.Project[]; query: string }
+  ) {
+    projects.forEach((project: todoist.Project) => {
+      const name = project.name.indexOf(' ') !== -1 ? `[${project.name}]` : project.name
 
-        this.items.push(
-          Item({
-            title: project.name,
-            subtitle: `Move task to ${project.name}`,
-            autocomplete: `${query.replace(/(^.*#).*/, '$1')}${name} `,
-            valid: false
-          })
-        )
-      })
-    }
+      this.items.push(
+        Item({
+          title: project.name,
+          subtitle: `Move task to ${project.name}`,
+          autocomplete: `${query.replace(/(^.*#).*/, '$1')}${name} `,
+          valid: false
+        })
+      )
+    })
   }
-)
+})
