8efa5f61143b10802b757cf595c03bdaea9f110a
Martien Oranje <martieno@gmail.com>
2020-02-26 23:30:32 +0100

    chore: update setup script

--- tools/setup.ts
+++ tools/setup.ts
@@ -1,6 +1,7 @@
 import inquirer from 'inquirer';
-import { mkdir, ln, rm, exec } from 'shelljs';
+import { mkdir, ln, rm } from 'shelljs';
 import { existsSync } from 'fs';
+import chalk from 'chalk';
 
 let dataPath = '';
 let cachePath = '';
@@ -67,17 +68,31 @@ inquirer
     },
   ])
   .then((answers: any) => {
-    // Setup a symlink from workflow folder pointing to dist/workflow in the repo
+    // Setup a symlink from workflow folder pointing to dist/workflow in the
+    // repo
     const uid = uuid();
-    mkdir('-p', `${cwd}/dist`);
-    ln('-s', `${cwd}/dist/workflow`, `${answers.path}/user.workflow.${uid}`);
+    let error = '';
+    error += mkdir('-p', `${cwd}/dist`).stderr ?? '';
+    error +=
+      ln('-s', `${cwd}/dist/workflow`, `${answers.path}/user.workflow.${uid}`)
+        .stderr ?? '';
 
-    mkdir('-p', cachePath);
-    mkdir('-p', dataPath);
-    rm('-rf', 'data', 'cache');
-    ln('-s', dataPath, `data`);
-    ln('-s', cachePath, `cache`);
+    error += mkdir('-p', cachePath).stderr ?? '';
+    error += mkdir('-p', dataPath).stderr ?? '';
+    error += rm('-rf', 'data', 'cache').stderr ?? '';
+    error += ln('-s', dataPath, `data`).stderr ?? '';
+    error += ln('-s', cachePath, `cache`).stderr ?? '';
+
+    if (error) throw new Error(error);
+
+    let folder = `${answers.path}/user.workflow.${uid}`;
+    console.info(
+      `${chalk.bold.green('Setup finished:')}\n\n${chalk.underline(
+        'dist/workflow'
+      )} is now linked to ${chalk.underline(folder)}`
+    );
   })
   .catch((err: Error) => {
-    console.error("That didn't go as planned\n", err);
+    console.error(`${chalk.bold.red('Setup failed:')}\n\n`, err);
   });
+// /Users/martien/MEGA/Projects/Alfred/Alfred.alfredpreferences/workflows
