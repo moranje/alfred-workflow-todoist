706b482d20d43b4e48e8653f0bfc525de4750d4e
Martien Oranje <martieno@gmail.com>
2020-02-27 10:09:20 +0100

    build: hopefully fix failing build

--- tools/sentry-release.sh
+++ tools/sentry-release.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 VERSION="$1"
+FIRST_COMMIT="$2"
+LAST_COMMIT="$3"
 
 npx sentry-cli releases new "$VERSION" --finalize
-# npx sentry-cli releases set-commits "$VERSION" --commit
-# moranje/alfred-workflow-todoist
+npx sentry-cli releases set-commits "$VERSION" --commit
+"moranje/alfred-workflow-todoist@$FIRST_COMMIT..$LAST_COMMIT"
 npx sentry-cli releases files "$VERSION" upload ./dist/workflow/alfred-workflow-todoist.js '~/alfred-workflow-todoist.js'
 npx sentry-cli releases files "$VERSION" upload-sourcemaps ./dist/workflow/alfred-workflow-todoist.js.map --rewrite
-# sentry-cli releases deploys "$VERSION" new -e production
+sentry-cli releases deploys "$VERSION" new -e production
